dfa9d2f5d2ebdcd13f611946a69484d3cd63cdc5
Zyxy1919cz <Zyxy1919cz@gmail.com>
2021-07-20 12:46:05 +0000

    Restored Live server, Add support for Sass
    Repaired Live server in Docker and repositioned .env file in to root for
    no need to copy it to container.
    Add dependency node-sass for compilation .scss file into React
    components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import Header from './components/Header.js';
+
+import './scss/main.scss';
+
 const App = () => {
     return(
         <div className="main">
-            <header className="header"></header>
-            <h1>`Test ${process.env.TESTING_VAR}`</h1>
-            <footer className="footer"></footer>
+            <Header />
+            <h1>Test some text {process.env.TESTING_VAR}</h1>
+            <p>Hello</p>
         </div>
     )
 };
---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Main header
+const Header = () => {
+    return (
+        <container className="header">
+            <div>
+                <p>Some text</p>
+            </div>
+        </container>
+    )
+};
+
+export default Header;
